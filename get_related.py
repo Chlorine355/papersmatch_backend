fa689a6a9bf254b25ae6019b4d5a69e7cbdb2bd2
Chlorine355 <andrey.akimov@abgroup.tech>
2025-06-09 02:55:53 +0300

    feat: add batch request, envs

---
+++ get_related.py
@@ -0,0 +1,96 @@
+import os
+
+import networkx as nx
+import requests
+
+
+api_key = os.environ["S2_API_KEY"]
+
+
+def get_related_papers(paper_id):
+    r = requests.get(f"https://api.semanticscholar.org/graph/v1/paper/{paper_id}?fields=title,abstract,year,authors,"
+                     f"fieldsOfStudy,"
+                     f"externalIds,"
+                     f"citationCount,citations.citationCount,citations.title,citations.abstract,citations.year,"
+                     f"citations.authors,citations.fieldsOfStudy,"
+                     "references.citationCount,references.title,references.abstract,references.year,"
+                     "references.authors,references.fieldsOfStudy",
+                     headers={'x-api-key': api_key}).json()
+    refs = r.get('references', [])
+    if refs is None:
+        refs = []
+    references = sorted(refs, key=lambda x: x['citationCount'] if x['citationCount'] else 0,
+                        reverse=True)[:20]
+    cits = r.get('citations', [])
+    if cits is None:
+        cits = []
+    citations = sorted(cits, key=lambda x: x['citationCount'] if x['citationCount'] else 0, reverse=True)[:20]
+
+    del r['references']
+    del r['citations']
+    return r, references + citations
+
+
+def get_related_batch(paper_ids):
+    if not paper_ids:
+        return []
+    try:
+        r = requests.post(
+            'https://api.semanticscholar.org/graph/v1/paper/batch',
+            params={'fields': "citations,references"},
+            json={"ids": paper_ids},
+            headers={'x-api-key': api_key}).json()
+        if type(r) == 'dict':
+            return []
+        return r
+    except Exception:
+        return []
+
+
+class GraphVisualization:
+    def __init__(self):
+        self.visual = []
+        self.nodes = []
+        self.origin = ''
+
+    def addEdge(self, a, b):
+        temp = [a, b]
+        self.visual.append(temp)
+
+    def addNode(self, a):
+        self.nodes.append(a)
+
+    def get_graph(self):
+        G = nx.Graph()
+        G.add_edges_from(self.visual)
+        G.add_nodes_from(self.nodes)
+        if len(G.nodes) < 5:
+            return G
+        for limit in range(15, -1, -1):
+            f = 1
+            while f:
+                f = 0
+                for node in list(G.nodes):
+                    if G.degree[node] < limit:
+                        f = 1
+                        G.remove_node(node)
+            if len(G.nodes) == 0:
+                G.add_edges_from(self.visual)
+            else:
+                if len(G.nodes) < 10 and len(G.nodes) != 1:
+                    G.clear()
+                    G.add_edges_from(self.visual)
+                    continue
+                break
+        T = nx.algorithms.minimum_spanning_tree(G)
+        G.clear()
+        G.add_edges_from(T.edges())
+        if len(G.nodes) > 35:
+            for adj_node in G[self.origin]:
+                if len(G[adj_node]) > 3:
+                    to_delete = []
+                    for adj_adj_node in G[adj_node]:
+                        to_delete.append(adj_adj_node)
+                    to_delete.remove(self.origin)
+                    G.remove_nodes_from(to_delete[:-3])
+        return G
--- server.py
+++ server.py
@@ -1,17 +1,18 @@
-from pprint import pprint
-
+import os
 from flask import Flask, request, jsonify
 import requests
 from flask_cors import CORS
 
+from get_related import get_related_papers, GraphVisualization, get_related_batch
 
 app = Flask(__name__)
 CORS(app)
 
 app.config['JSON_AS_ASCII'] = False
-app.config['SECRET_KEY'] = ''
+app.config['SECRET_KEY'] = os.environ['PM_SECRET_KEY']
 
-api_key = ""
+api_key = os.environ["S2_API_KEY"]
+
 
 PAGE_SIZE = 20
 
@@ -39,7 +40,6 @@ def search():
         "year": year_range,
         "minCitationCount": min_citations if min_citations else "0",
     }
-    print(params)
     response = requests.get(
         f"https://api.semanticscholar.org/graph/v1/paper/search{is_open_access}",
         params=params,
@@ -49,6 +49,44 @@ def search():
     results = response.get("data", [])
     total = response.get("total", 0)
     return jsonify({"data": results, "total": min(total, 1000)})
+
+
+@app.route('/graph/<paper_id>', methods=['GET'])
+def graph(paper_id):
+    origin, related_to_root_list = get_related_papers(paper_id)
+    G = GraphVisualization()
+    id_to_paper = {paper_id: origin}
+    G.addNode(paper_id)
+    G.origin = paper_id
+    related_to_root_list = [rel for rel in related_to_root_list if rel['paperId']]
+    for rel in related_to_root_list:
+        id_to_paper[rel['paperId']] = rel  # save
+        G.addEdge(paper_id, rel["paperId"])
+    related_to_root_ids = [rel['paperId'] for rel in related_to_root_list]
+    newrels = get_related_batch(related_to_root_ids)
+    for i in range(len(newrels)):
+        if newrels[i].get("citations"):
+            for citation in newrels[i]['citations']:
+                if citation['paperId']:
+                    id_to_paper[citation['paperId']] = citation
+                    G.addEdge(related_to_root_ids[i], citation['paperId'])
+        if newrels[i].get("references"):
+            for reference in newrels[i].get('references', []):
+                if reference['paperId']:
+                    id_to_paper[reference['paperId']] = reference
+                    G.addEdge(related_to_root_ids[i], reference['paperId'])
+    gr = G.get_graph()
+    node_list = list(gr.nodes)
+    all_keys = list(id_to_paper.keys())
+    for key in all_keys:
+        if key not in node_list:
+            del id_to_paper[key]
+    return jsonify({"nodes": list(id_to_paper.values()), "edges": list(gr.edges)})
+
+
+@app.route('/add_to_favourites', methods=['POST'])
+def add_to_favourites():
+    return 0
 
 
 app.run('0.0.0.0', 80)
